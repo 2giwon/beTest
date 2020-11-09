ac308f950ca38ac91db58d1d9b83d47261283bb3
2giwon <2giwon@gmail.com>
2020-11-09 16:55:22 +0900

    [수정] - 현재 선택 아티스트 id를 viewModel에서 들고 있도록 수정

--- app/src/main/java/com/egiwon/benxtest/shop/ShopViewModel.kt
+++ app/src/main/java/com/egiwon/benxtest/shop/ShopViewModel.kt
@@ -8,6 +8,7 @@ import com.egiwon.benxtest.base.BaseViewModel
 import com.egiwon.benxtest.data.ShopRepository
 import com.egiwon.benxtest.data.entity.ShopInfoResponse
 import com.egiwon.benxtest.shop.artist.ARTIST
+import com.egiwon.benxtest.shop.model.Artist
 import com.egiwon.benxtest.shop.model.Banner
 import com.egiwon.benxtest.shop.model.Notice
 import com.egiwon.benxtest.shop.model.SaleItem
@@ -25,9 +26,6 @@ class ShopViewModel @ViewModelInject constructor(
     private val _loadingBar = MutableLiveData<Boolean>()
     val loadingBar: LiveData<Boolean> get() = _loadingBar
 
-//    private val _shopInfo = MutableLiveData<ShopInfo>()
-//    val shopInfo: LiveData<ShopInfo> get() = _shopInfo
-
     private val _artistId = MutableLiveData<Int>()
     val artistId: LiveData<Int> get() = _artistId
 
@@ -43,10 +41,15 @@ class ShopViewModel @ViewModelInject constructor(
     private val _noticeItems = MutableLiveData<List<Notice>>()
     val noticeItems: LiveData<List<Notice>> get() = _noticeItems
 
+    private val _artists = MutableLiveData<List<Artist>>()
+    val artists: LiveData<List<Artist>> get() = _artists
+
     private val _map = HashMap<Int, List<SaleItem>>()
 
-    fun loadShopInfo(artistId: Int = ARTIST.BTS.value) {
-        repository.loadShopInfo(artistId)
+    private var _currentArtistId = ARTIST.BTS.value
+
+    fun loadShopInfo() {
+        repository.loadShopInfo(_currentArtistId)
             .doOnSubscribe { _loadingBar.postValue(true) }
             .doAfterTerminate { _loadingBar.postValue(false) }
             .subscribeOn(Schedulers.io())
@@ -54,14 +57,19 @@ class ShopViewModel @ViewModelInject constructor(
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeBy(
                 onSuccess = {
-                    _artistId.value = artistId
                     _banners.value = it.banners
                     _shopItems.value = it.shopItems
                     _noticeItems.value = it.notices
+                    _artists.value = it.artist
+                    findCurrentArtist(it.artist, _currentArtistId)
                 },
                 onError = { toastMessageMutableLiveData.value = R.string.error_load_fail_shop_info }
             )
             .addTo(compositeDisposable)
+    }
+
+    private fun findCurrentArtist(artists: List<Artist>, artistId: Int) {
+        _artistId.value = artists.find { it.id == artistId }?.id
     }
 
     fun getShopItems(categoryId: Int) {
